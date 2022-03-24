0c2a1bf6c9647ae23f257af95c59b6fae3ddc078
Anup <92488889+anuplotter@users.noreply.github.com>
2022-03-24 13:25:48 +0530

    Updating excel dashboard

--- census.py
+++ census.py
@@ -29,7 +29,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+#st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 # Formatting link: https://d3-wiki.readthedocs.io/zh_CN/master/Formatting/
